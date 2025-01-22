85a87c47f19373a8c3ec0db1f59ff15b0cc2ac44
Eugene Safonow <yauheni.safonau@innowise.com>
2025-01-22 16:01:49 +0100

    feat: add config service

--- innogram/src/main.ts
+++ innogram/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const config = app.get(ConfigService);
+  const port = config.get('PORT') || 3000;
+
+  await app.listen(port, () => {
+    console.log(`[Server started on port ${port}]`);
+  });
 }
 bootstrap();
